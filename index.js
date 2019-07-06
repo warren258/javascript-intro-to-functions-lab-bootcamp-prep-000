b8e7d0c8be66233195d9ecb0ab2333de36f23b74
Warren McDonald <warrenjmac@mail.com>
2019-07-06 05:17:45 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,19 @@
+function shout(string){
+  return string.toUpperCase()
+}
+function whisper(string){
+  return string.toLowerCase()
+}
+function logShout(string){
+  console.log(string.toUpperCase())
+}
+function logWhisper(string){
+  console.log(string.toLowerCase())
+}
+var uppercase = "Hello !"
+var lowercase = "hello !"
+function sayHiToGrandma(){
+if (string === string.toLowerCase){
+  return ("I can\'t hear you!")
+}
+}
